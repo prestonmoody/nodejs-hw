8c67f7ed56af5063ba0f3a2090c476fcedb34fce
Chandradeo Arya <chandradeoarya@air.local>
2022-06-12 10:22:59 +0300

    adding clg

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000; //EB will pick default env value 8080 if not mentioned in configuration.
 
-app.get('/', (req, res) => res.send('<h1>Hello DevOps on dev!</h1>'));
+app.get('/', (req, res) => {
+    console.log("New get")
+    res.send('<h1>Hello DevOps on dev!</h1>');
+    });
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
